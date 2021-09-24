adcafc0e7d8bcda1322e1c3b773070d0d9b00f82
nicolasarruda <nlarruda@hotmail.com>
2021-09-24 16:10:01 -0300

    Created README.md file

--- src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.exception.PersonNotFoundException;
@@ -23,14 +24,10 @@ import one.digitalinnovation.personapi.services.PersonService;
 
 @RestController
 @RequestMapping(value = "/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
 	private PersonService personService;
-	
-	@Autowired
-	public PersonController(PersonService personService) {
-		this.personService = personService;
-	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
--- src/main/java/one/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/services/PersonService.java
@@ -1,7 +1,6 @@
 package one.digitalinnovation.personapi.services;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -9,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
@@ -17,16 +17,12 @@ import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repositories.PersonRepository;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService  {
 
 	private PersonRepository personRepository;
 	
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-	@Autowired
-	public PersonService(PersonRepository personRepository) {
-		this.personRepository = personRepository;
-	}
 	
 	public MessageResponseDTO createPerson(PersonDTO personDTO) {
 		Person personToSave = personMapper.toModel(personDTO);
